4b55712e0a465747e6186a29e89669be31bbd514
Thota Charitha <131519785+charitha3030@users.noreply.github.com>
2023-06-24 13:20:37 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,12 @@
+#Random Password Generator in Python
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+length = int(input("Enter the desired length of the password: "))
+password = generate_password(length)
+print("Generated Password:", password)
